63877213ff212c15f7fad2a17d8833bd33f895e6
DAVID ZAGACETA <zagaceta@br018.pvt.bridges.psc.edu>
2020-02-12 11:21:20 -0500

    blocking computed with avx intrinsics to compute partial column vectors using icc

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -5,18 +5,19 @@ COMPILER= gnu
 
     Please include All compiler flags and libraries as you want them run. You can simply copy this over from the Makefile's first few lines
  
-CC = cc
-OPT = -O3
+CC = icc
+OPT = -O3 -mavx2
 CFLAGS = -Wall -std=gnu99 $(OPT)
 MKLROOT = /opt/intel/composer_xe_2013.1.117/mkl
 LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKLROOT)/lib/intel64/libmkl_sequential.a $(MKLROOT)/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm
 
 */
+#include <immintrin.h>
 
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 41
+#define BLOCK_SIZE 64
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -27,14 +28,41 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; ++i)
+  int numUnrolls = 8;
+  int i,j,k,n;
+  double cij;
+  __m256d a,b;
+  __m256d c[numUnrolls];
+  // Taking into account the 4 data points loaded from SIMD
+  // and the number of loops we are unrolling
+  for (i = 0; i < M-M%(4*numUnrolls); i+=4*numUnrolls){
     /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      for (int k = 0; k < K; ++k)
-	cij += A[i+k*lda] * B[k+j*lda];
+    for (j = 0; j < N; ++j){
+      /* Get C(i,j) */
+      for (n=0; n<numUnrolls; ++n)
+        c[n] = _mm256_loadu_pd(C+i+j*lda+n*4); 
+      /* Product of a partial column of A
+         With an element of B
+         to get a partial sum of 1 column
+         of C since we are in column major*/
+      for (k = 0; k < K; ++k){
+        b = _mm256_broadcast_sd(B+k+j*lda);
+        for (n=0; n<numUnrolls; ++n){
+          a = _mm256_loadu_pd(A+i+k*lda+n*4);
+          c[n] = _mm256_fmadd_pd(b,a,c[n]);
+          }
+        }
+      for (n=0; n<numUnrolls; ++n)
+        _mm256_store_pd(C+i+j*lda+n*4, c[n]);
+    }
+  }
+
+  // clean up remaining elements with naive matrix mult
+  for ( ; i<M; ++i)
+    for (j=0; j<N; ++j){
+      cij = C[i+j*lda];
+      for (k=0; k<K; ++k)
+        cij += A[i+k*lda] * B[k+j*lda];
       C[i+j*lda] = cij;
     }
 }
